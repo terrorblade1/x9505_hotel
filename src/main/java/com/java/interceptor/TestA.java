46c3f9b578dceb1dd42cb4586689f45048fd6370
terrorblade1 <1412968676@qq.com>
2020-04-14 21:36:28 +0800

    创建测试类TestA, 第1次提交

---
+++ src/main/java/com/java/interceptor/TestA.java
@@ -0,0 +1,11 @@
+package com.java.interceptor;
+
+/**
+ * Author: 乐科
+ * Date: 2020/4/14 20:11
+ */
+public class TestA {
+    public static void main(String[] args) {
+        System.out.println("TestA");
+    }
+}
